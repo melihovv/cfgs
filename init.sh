d649a00d1b77ce6477df2487af9429721167ff04
Alexander Melihov <amelihovv@ya.ru>
2015-07-02 16:44:38 +0300

    Added init.sh

---
+++ init.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+SCRIPT_PATH=$(cd `dirname "${BASH_SOURCE[0]}"` && pwd)/`basename "${BASH_SOURCE[0]}"`
+SCRIPT_DIR="`dirname "${SCRIPT_PATH}"`"
+
+ln -s "$SCRIPT_DIR/.vimrc" "$HOME/.vimrc"
+
+ln -s "$SCRIPT_DIR/.bash_aliases" "$HOME/.bash_aliases"
+
+ln -s "$SCRIPT_DIR/.gitconfig" "$HOME/.gitconfig"
+ln -s "$SCRIPT_DIR/.gitignore_global" "$HOME/.gitignore_global"
+git config --global core.excludesfile $HOME/.gitignore_global
+
+if [ -h "$HOME/.vim" ];
+then
+    rm -rf "$HOME/.vim"
+fi
+
+ln -s "$SCRIPT_DIR/.vim" "$HOME/.vim"
+
+cd "${SCRIPT_DIR}"
+git submodule init && git submodule update
