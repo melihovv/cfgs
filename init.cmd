2fa99b0219177191514ee047fae0fdb0dab0b4e4
Alexander Melihov <amelihovv@ya.ru>
2015-07-02 15:11:16 +0300

    Updated init.cmd

--- init.cmd
+++ init.cmd
@@ -3,16 +3,19 @@
 @if not exist "%HOME%" @set HOME=%HOMEDRIVE%%HOMEPATH%
 @if not exist "%HOME%" @set HOME=%USERPROFILE%
 
-mklink "%HOME%.vimrc" "%cd%\.vimrc"
+set wd=%~dp0
 
-mklink "%HOME%.bashrc" "%cd%\.bashrc"
-mklink "%HOME%.bash_aliases" "%cd%\.bash_aliases"
+mklink "%HOME%.vimrc" "%wd%\.vimrc"
 
-mklink "%HOME%.gitconfig" "%cd%\.gitconfig"
-mklink "%HOME%.gitignore_global" "%cd%\.gitignore_global"
-git config --global core.excludesfile ~/.gitignore_global
+mklink "%HOME%.bashrc" "%wd%\.bashrc"
+mklink "%HOME%.bash_aliases" "%wd%\.bash_aliases"
 
-mklink /D "%HOME%vimfiles" "%cd%\.vim\"
+mklink "%HOME%.gitconfig" "%wd%\.gitconfig"
+mklink "%HOME%.gitignore_global" "%wd%\.gitignore_global"
+git config --global core.excludesfile "%HOME%.gitignore_global"
 
+mklink /D "%HOME%vimfiles" "%wd%.vim\"
+
+cd "%wd%"
 git submodule init && git submodule update
 
